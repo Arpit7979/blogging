33a759fa5599dac93e8af0181f7f75dc7eb168c4
Arpit7979 <arpitbuxar79@gmail.com>
2025-04-30 00:56:32 +0530

    fixed cookie

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -56,7 +56,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV ? "strict" : "none",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
